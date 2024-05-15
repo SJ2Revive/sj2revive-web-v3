0574cc32481fbc747dcda62a29e25e059f2ac277
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-05-15 23:50:16 +0200

    dodalem placeholder zeby nie wywalalo 404

---
+++ admin/modpost.php
@@ -0,0 +1,46 @@
+<?php
+require("../src/whitelist.php");
+if(!checkifwhitelisted())
+{
+    header("location: /");
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SJ2Revive</title>
+    <style>
+        @import url("../static/css/blogpost.css");
+    </style>
+</head>
+<body>
+    <div class="sidebar">
+        <h2 class="logo">
+            <span style="color: #086ed3;">SJ2</span>
+            <span style="color: green;">Revive</span>
+            <span style="color: yellow;">.top</span>
+        </h2>
+        <p style="font-size: 12px;text-align: center;color: yellowgreen;">Jedyna strona z modami do <span style="color: #086ed3;">Symulatora Jazdy 2</span></p>
+        <a href="/">Strona Główna</a>
+        <a href="/faq.php">FAQ</a>
+        <a href="/nowosci.php">Nowości</a>
+        <a href="/modyfikacje.php">Modyfikacje</a>
+        <a href="/launcher.php">Launcher</a>
+        <a href="/oa.php">Aktualizacje</a>
+        <a href="/ustawienia.php">Ustawienia</a>
+        <?php
+        render();
+        ?>
+    </div>
+    <div class="main-content">
+        <br/>
+        <br/>
+    <form action="api/v1/mods/add.php" method="get">
+        TODO
+    </form>
+    </div>
+</body>
+</html>
